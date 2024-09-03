bb816aca5983f2a3c6dfb4050ce125e9244a98c0
Jenseko <jenscode23@gmail.com>
2024-09-03 22:46:54 +0200

    implementing fetching with using useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,10 +50,20 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
+const KEY = "1acc6a47";
 
-  const [watched, setWatched] = useState(tempWatchedData);
+export default function App() {
+  const [movies, setMovies] = useState([]);
+
+  const [watched, setWatched] = useState([]);
+
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=predator`)
+      .then((res) => res.json())
+      .then((data) => setMovies(data.Search));
+  }, []);
+
+  // -- Not allowed in render logic --
 
   // Component composition
   return (
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import "./index.css";
-//import App from "./App";
-import { useState } from "react";
+import "./index.css";
+import App from "./App";
+// import { useState } from "react";
 
-import StarRating from "./StarRating";
+// import StarRating from "./StarRating";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating color="purple" maxRating={10} onSetRating={setMovieRating} />;
-      <p>This Movie was rated {movieRating} Stars!</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating color="purple" maxRating={10} onSetRating={setMovieRating} />;
+//       <p>This Movie was rated {movieRating} Stars!</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={12}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={4} />
-    <Test />
+    <Test /> */}
   </React.StrictMode>
 );
